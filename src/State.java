e901efff9b89b6ab0565ff68745cbf75615e5f27
immortal2612 <31737415+immortal2612@users.noreply.github.com>
2019-03-26 12:24:49 -0700

    creating state

---
+++ src/State.java
@@ -0,0 +1,10 @@
+import java.util.ArrayList;
+
+public class State {
+    String name;
+    ArrayList<County> counties;
+    public State(String n, ArrayList<County> c){
+        name = n;
+        counties = c;
+    }
+}
