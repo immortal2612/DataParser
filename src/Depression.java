be5df365c1bb42b37a75585df480dab26b03a9f7
immortal2612 <31737415+immortal2612@users.noreply.github.com>
2019-03-27 13:33:49 -0700

    finished theoretical depression object

---
+++ src/Depression.java
@@ -0,0 +1,19 @@
+public class Depression {
+    String name = null;
+    int count = 0;
+
+    public Depression(String n, int c){
+        name = n;
+        count = c;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public int getCount() {
+        return count;
+    }
+
+
+}
